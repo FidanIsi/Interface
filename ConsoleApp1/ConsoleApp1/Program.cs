edec925b8ccf2175e04fdd7811e82af56b2778f8
fidan <fidanji@code.edu.az>
2023-08-22 15:53:52 +0400

    new task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    interface IPerson
+    {
+        void Speak();
+    }
+
+    public class RussianPerson : IPerson
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Privet");
+        }
+    }
+
+    public class FrenchPerson : IPerson
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Bonjour");
+        }
+    }
+
+    public class SpanishPerson : IPerson
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var RussianPerson = new RussianPerson();
+            var FrenchPerson = new FrenchPerson();
+            var SpanishPerson = new SpanishPerson();
+            RussianPerson.Speak();
+            FrenchPerson.Speak();
+            SpanishPerson.Speak();
+        }
+        
+    }
+}
---
+++ Static/Static/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.AccessControl;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area1 = GeometryHelper.triangle(3, 7);
+            Console.WriteLine(area1);
+
+            double area2 = GeometryHelper.rectangle(4.6, 15);
+            Console.WriteLine(area2);
+
+            double area3 = GeometryHelper.circle(4.6);
+            Console.WriteLine(area3);
+        }
+    }
+
+    public static class GeometryHelper
+    {
+        public static double triangle(double width, double length)
+        {
+            double area = width * length / 2;
+            return area;
+        }
+
+        public static double rectangle (double width, double height)
+        {
+            double area = width * height;
+            return area;
+        }
+
+        public static double circle (double rad)
+        {
+            double area = Math.PI * rad * rad;
+            return area;
+        }
+    }
+}
